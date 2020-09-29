4ca8721f46272ef7a2519d40adec10aba4d40f96
elbeg9amar <elbeg9amar@yahoo.com>
2020-09-28 20:52:12 -0400

    added getusers by id and post new user and delete user

--- node.js
+++ node.js
@@ -3,6 +3,7 @@ const id = require('short-id');
 const { Server } = require('http');
 
 const server = express();
+server.use(express.json());
 
 
 server.get('/', (req, res) => {
@@ -22,10 +23,29 @@ let users = [
     }
 ] 
 
-server.get('/users', (req, res) => {
-    res.status(200).json({data: users})
+server.get('/api/users', (req, res) => {
+    res.status(200).json({data: users});
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    const found = users.find(user => user.id == id)
+    res.status(200).json({date: found})
+});
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+    users.push({id: id.generate(), ...newUser})
+    res.status(201).json({data: users})
+});
+
+server.delete('/api/users/:id', (req,res) => {
+     const id = req.params.id
+     const deletedUser = users.find(user => user.id == id)
+     users = users.filter(user => user.id !== id)
+     res.status(200).json({data: deletedUser})
 })
 
 const port = 999;
 
-server.listen(port, () => console.log("API running ..."));
+server.listen(port, () => console.log(`API running ... ${port}`));
